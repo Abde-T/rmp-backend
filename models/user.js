f6762b98c701bc763b730902c6bee5a12a8379c8
Abderrahmane <tiamani3939@gmail.com>
2023-07-21 12:20:25 +0100

    user details

--- models/user.js
+++ models/user.js
@@ -4,12 +4,13 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required:  true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  linkedin: { type: String, required: true },
-  gitHub:  { type: String, required: true },
-  website: { type: String, required: true },
+  linkedin: { type: String, required: false },
+  gitHub:  { type: String, required: false },
+  website: { type: String, required: false },
   selectedFile: { type: String, required: false },
   id: { type: String },
 });
+
 
 const User = mongoose.model('User', userSchema)
 export default User;
